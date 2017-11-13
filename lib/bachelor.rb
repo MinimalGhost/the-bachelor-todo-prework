8d41c1b604c286ac4a5ce5dca633d616fdbd1e5b
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-13 03:24:36 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -55,7 +55,8 @@ def get_average_age_for_season(data, season)
   data.each do |k, v|
     if season == k
       v.each do |contestant|
-        binding.pry
+        sum += contestant["age"].to_i
+        counter += 1
       end
     end
   end
