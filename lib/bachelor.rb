06b94f57894463162be096ebdf23c324bb083c5e
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-13 02:20:47 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -14,17 +14,49 @@ def get_first_name_of_season_winner(data, season)
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  data.each do |k, v|
+    v.each do |contestant|
+      if contestant["occupation"] == occupation
+        return contestant["name"]
+      elsif contestant["occupation"] == occupation
+        return contestant["name"]
+      elsif contestant["occupation"] == occupation
+        return contestant["name"]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+  data.each do |k, v|
+    v.each do |contestant|
+      if contestant["hometown"] == hometown
+        counter += 1
+      end
+    end
+  end
+  return counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+  data.each do |k, v|
+    v.each do |contestant|
+      if contestant["hometown"] == hometown
+        return contestant["occupation"]
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  sum = 0
+  count = 0
+  season.each do |k, v|
+      # v.each do |contestant|
+      # sum += contestant["age"]
+      # count += 1
+    end
+  end
+  # return sum / count
 end
